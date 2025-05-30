6fc8b17f3c33385f610fb770fc9337b645e62ca5
Abdoulaye Ba <abdoulaye_ba@outlook.com>
2025-05-30 12:19:22 +0200

    adding loggedin or not loggedin functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
-import React, { useEffect } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import ProductList from './pages/ProductList';
-import ProductCreate from './pages/ProductCreate';
-import ProductEdit from './pages/ProductEdit';
-import UserList from './pages/UserList';
-import UserCreate from './pages/UserCreate';
-import UserEdit from './pages/UserEdit';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import './App.css';
+import React, { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import ProductList from "./pages/ProductList";
+import ProductCreate from "./pages/ProductCreate";
+import ProductEdit from "./pages/ProductEdit";
+import UserList from "./pages/UserList";
+import UserCreate from "./pages/UserCreate";
+import UserEdit from "./pages/UserEdit";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import "./App.css";
 
 export default function App() {
   useEffect(() => {
-    document.title = 'E-commerce App';
+    document.title = "E-commerce App";
   }, []);
 
   return (
@@ -20,6 +20,14 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route
+          path="/products"
+          element={isLoggedIn() ? <ProductList /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/orders"
+          element={isLoggedIn() ? <OrderList /> : <Navigate to="/login" />}
+        />
         <Route path="/products" element={<ProductList />} />
         <Route path="/products/new" element={<ProductCreate />} />
         <Route path="/products/:id/edit" element={<ProductEdit />} />
@@ -27,10 +35,15 @@ export default function App() {
         <Route path="/users/new" element={<UserCreate />} />
         <Route path="/users/:id/edit" element={<UserEdit />} />
         {/* Placeholder for orders */}
-        <Route path="/orders" element={<div className='p-8 text-2xl text-gray-700'>Gestion des commandes à venir...</div>} />
+        <Route
+          path="/orders"
+          element={
+            <div className="p-8 text-2xl text-gray-700">
+              Gestion des commandes à venir...
+            </div>
+          }
+        />
       </Routes>
     </>
   );
 }
-
-
